650257d1f6aa467f4b1e1272e31a8eff71bd201e
unknown <udaypolkampally@gmail.com>
2023-07-11 23:48:48 +0530

    Wureka server

--- ConfigServer/src/main/java/org/healthcare/configserver/ConfigServer.java
+++ ConfigServer/src/main/java/org/healthcare/configserver/ConfigServer.java
@@ -1,13 +1,15 @@
-package org.healthcare.user;
+package org.healthcare.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class UserApplication {
+public class ConfigServer {
   public static void main(String[] args) {
-    SpringApplication.run(UserApplication.class, args);
+    SpringApplication.run(ConfigServer.class, args);
   }
 }
